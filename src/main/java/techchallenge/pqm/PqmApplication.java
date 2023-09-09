4a5e40d0cd08f2ce5a77a15f2eb07551ce9d5ab2
Lucas Peixoto de Alencar Rocha <lucas.peixoto.14@gmail.com>
2023-09-09 14:17:40 -0300

    Spring init

---
+++ src/main/java/techchallenge/pqm/PqmApplication.java
@@ -0,0 +1,13 @@
+package techchallenge.pqm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PqmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PqmApplication.class, args);
+    }
+
+}
---
+++ src/test/java/techchallenge/pqm/PqmApplicationTests.java
@@ -0,0 +1,13 @@
+package techchallenge.pqm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PqmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
